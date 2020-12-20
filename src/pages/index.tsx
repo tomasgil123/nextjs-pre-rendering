8b42be6968e363773834d7bc82a0328de70a01c4
Tomas Gil <tomasgil1234@gmail.com>
2020-12-20 12:21:37 -0300

    feat: add list of sections with img

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import React, { FC } from 'react'
+import { useRouter } from 'next/router'
 
 import MainLayout from 'src/layouts/main'
 
@@ -6,7 +7,33 @@ import MainLayout from 'src/layouts/main'
 import PageWithLayout from 'src/types/pageWithLayout'
 
 const Home: FC = () => {
-  return <div>Home</div>
+  const router = useRouter()
+
+  const sections = [
+    { name: 'Section 1', show: true, img: '/section-1.jpg' },
+    { name: 'Section 2', show: true, img: '/section-2.jpg' },
+    { name: 'Section 3', show: true, img: '/section-3.jpg' },
+    { name: 'Section 4', show: true, img: '/section-4.jpg' },
+  ]
+
+  const sectionsFiltered = sections.filter((section) => section.show)
+
+  return (
+    <div>
+      Home
+      <div>
+        {sectionsFiltered.map((section) => (
+          <div
+            key={section.name}
+            style={{ padding: '10px', margin: '10px', border: '1px solid black', width: '450px' }}
+          >
+            <div>{section.name}</div>
+            <img style={{ width: '400px', height: '400px' }} src={section.img} alt={section.name} />
+          </div>
+        ))}
+      </div>
+    </div>
+  )
 }
 
 ;(Home as PageWithLayout).layout = MainLayout
